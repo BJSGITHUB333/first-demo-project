f65232a5376f05dc91b865cd64bdf13f2cf1c358
BJSGITHUB333 <96619497+BJSGITHUB333@users.noreply.github.com>
2022-02-11 15:37:17 +0530

    Update App.java

--- first-demo-project/src/main/java/com/democompany/first_demo_project/App.java
+++ first-demo-project/src/main/java/com/democompany/first_demo_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "welcome to the devops training!" );
+        System.out.println( "welcome to this devops training!" );
     }
 }
